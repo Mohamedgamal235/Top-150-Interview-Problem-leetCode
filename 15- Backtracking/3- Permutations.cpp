2d85ef843e04ab6d736869203fad93e07b321407
Mohamed Gamal Ali <gemmymo6@gmail.com>
2025-03-10 03:03:07 +0200

    Problem Solution

--- 15- Backtracking/3- Permutations.cpp
+++ 15- Backtracking/3- Permutations.cpp
@@ -1,1 +1,22 @@
-s
+class Solution {
+    public:
+    vector<vector<int>> ans ;
+
+    void solve(vector<int>& nums , int index) {
+        if (index == nums.size()) {
+            ans.push_back(nums) ;
+            return;
+        }
+
+        for (int i = index ; i < nums.size() ; i++) { // for options
+            swap(nums[index] , nums[i]); // do
+            solve(nums , index + 1 ); // recurese
+            swap(nums[index] , nums[i]) ;  // undo
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        solve(nums , 0) ;
+        return ans ;
+    }
+};
