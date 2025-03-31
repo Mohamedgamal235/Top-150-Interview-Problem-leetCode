1f53a78b888cf5a5b313fc35486d2afbd0e42613
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-31 12:09:55 +0200

    Problem Solution

--- 05- Hashmap/C++/2- Isomorphic Strings.cpp
+++ 05- Hashmap/C++/2- Isomorphic Strings.cpp
@@ -1,1 +1,19 @@
-Isomorphic Strings
+class Solution {
+    public:
+        bool isIsomorphic(string s, string t) {
+            unordered_map<char , char> mpS , mpT ;
+    
+            for (int i = 0 ; i < s.size() ; i++) {
+    
+                if (mpS.count(s[i]) && mpS[s[i]] != t[i])
+                    return false ;
+                if (mpT.count(t[i]) && mpT[t[i]] != s[i])
+                    return false ;
+    
+                mpS[s[i]] = t[i] ;
+                mpT[t[i]] = s[i] ;
+            }
+    
+            return true ;
+        }
+    };
