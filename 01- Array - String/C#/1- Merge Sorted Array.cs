ff7f3d9316dc67f582f1513dda0dfdcaa63e9cf0
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-04-07 20:13:11 +0200

    Problem Solution

--- 01- Array - String/C#/1- Merge Sorted Array.cs
+++ 01- Array - String/C#/1- Merge Sorted Array.cs
@@ -3,6 +3,6 @@ public class Solution {
         for (int j = 0 , i = m ; j < n ; j++ , i++) 
                 nums1[i] = nums2[j] ;
     
-            sort(nums1.begin() , nums1.end());
+            Array.Sort(nums1);
     }
 }
